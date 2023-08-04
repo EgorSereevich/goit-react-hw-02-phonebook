8d1b82d88936b59b459453bf138ee67553f9004e
Egor Sergeevich <122815068+EgorSereevich@users.noreply.github.com>
2023-08-04 16:26:16 +0300

    first_commite_me

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,29 +1,66 @@
-import React from 'react';
-import { Component } from 'react';
-import { FormPhonebook } from './FormPhonebook/FormPhonebook';
-import { ContactsList } from './Contacts/Contacts';
+import { React, Component } from 'react';
+import { nanoid } from 'nanoid';
+import { AddContacts } from './AddContacts/AddContacts';
+import { Filter } from './Filter/Filter';
+import { Contacts } from './Contacts/Contacts';
+import { Container, Header, SecondHeader } from './App.styled';
+
 class App extends Component {
   state = {
-    contacts: [ {id: 'id-1', name: 'Rosie Simpson', number: '459-12-56'},
-    {id: 'id-2', name: 'Hermione Kline', number: '443-89-12'},
-    {id: 'id-3', name: 'Eden Clements', number: '645-17-79'},],
-    name: '',
+    contacts: [
+      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+    ],
+    filter: '',
   };
-  // addContact = (evt, state) => {
-  //   this.setState(prevState => {
-  //     console.log(state.contacts);
-  //     return {
-  //       [state.contacts]: evt.target.elements.name.value,
-        
-  //     };
-  //   });
-  // };
+  onSubmitForm = ({ name, number }) => {
+    const normalizedName = name.toLowerCase();
+    const contact = {
+      id: nanoid(),
+      name: name,
+      number: number,
+    };
+    let add = true;
+    this.state.contacts.forEach(el => {
+      if (normalizedName === el.name) {
+        alert(`${el.name}is already in contacts`);
+        add = false;
+      }
+    });
+    if (add) {
+      this.setState(prevState => ({
+        contacts: [contact, ...prevState.contacts],
+      }));
+    }
+  };
+  handleInputFilter = evt => {
+    evt.preventDefault();
+    this.setState({ filter: evt.target.value });
+  };
+  delContact = contId => {
+    this.setState(prevState => ({
+      contacts: prevState.contacts.filter(contact => contact.id !== contId),
+    }));
+  };
   render() {
+    const { filter } = this.state;
+    const normalizedFilter = filter.toLowerCase();
+    const visibleContacts = this.state.contacts.filter(contact =>
+      contact.name.toLowerCase().includes(normalizedFilter)
+    );
     return (
-      <div>
-        <FormPhonebook addContact={this.addContact} />
-        <ContactsList contacts={this.state.contacts} />
-      </div>
+      <Container>
+        <Header>Phonebook</Header>
+        <AddContacts onSubmit={this.onSubmitForm} />
+        <SecondHeader>Contacts</SecondHeader>
+        <Filter handleInputFilter={this.handleInputFilter} />
+        <Contacts
+          visibleContacts={visibleContacts}
+          delContact={this.delContact}
+        />
+      </Container>
     );
   }
 }
--- src/components/Contacts/Contacts.jsx
+++ src/components/Contacts/Contacts.jsx
@@ -1,18 +1,31 @@
-import { number } from 'prop-types';
-import React from 'react';
-
-export const ContactsList = ({contacts}) => {
+import { React } from 'react';
+import PropTypes from 'prop-types';
+import { Ul, Li, SpanName, SpanNumber, Button } from './Contacts.styled';
+export const Contacts = ({ visibleContacts, delContact }) => {
   return (
-    <div>
-      <h1>Contacts</h1>
-      <ul>
-        {contacts.map(contact => {
-          return (
-            <li key={contact.id}>{contact.name}:{contact.number}</li>
-          )
-          
-        })}
-      </ul>
-    </div>
+    <Ul>
+      {visibleContacts.map(({ id, name, number }) => {
+        return (
+          <Li key={id}>
+            <SpanName>{name}:</SpanName>
+            <SpanNumber>{number}</SpanNumber>
+            <Button type="button" onClick={() => delContact(id)}>
+              Delete
+            </Button>
+          </Li>
+        );
+      })}
+    </Ul>
   );
+};
+
+Contacts.propTypes = {
+  delContact: PropTypes.func.isRequired,
+  visibleContacts: PropTypes.arrayOf(
+    PropTypes.exact({
+      number: PropTypes.string.isRequired,
+      name: PropTypes.string.isRequired,
+      id: PropTypes.string.isRequired,
+    })
+  ).isRequired,
 };
